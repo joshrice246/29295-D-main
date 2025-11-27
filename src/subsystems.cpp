a646e9e953b8fa2a487ed31c41cea0d1458b1139
joshrice246 <joshrice246@ed.amdsb.ca>
2025-11-27 15:51:00 -0500

    new ports  1.4.1

--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -3,7 +3,7 @@
 
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
+    {1, 2, 3},    // Left Chassis Ports (negative port will reverse it!)
     {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
@@ -13,3 +13,39 @@ ez::Drive chassis(
 
 pros::Motor intake(7);
 
+pros::Motor belt(8);
+
+void SetDrive(int driveDistance, int driveSpeed, int turnDegrees, int turnSpeed, char driveMeasurment)
+{   
+    #pragma region setup
+    int basicMeasurment = 1; // 1 = inches change later if needed for different measurments
+    int leftPID;
+    int rightPID; 
+    int distance;
+    int turnDistance;
+    int wheelDiamiter = 3.25; // Wheel diameter of robot
+    int botWidth = 24; // Wheel to wheel width of robot
+    #pragma endregion
+
+    distance = driveDistance * basicMeasurment;
+    turnDistance = turnDegrees * botWidth / (2 * wheelDiamiter);
+
+    if (turnDegrees != 0)
+    {
+        leftPID = turnDistance;
+        rightPID = -turnDistance;
+    }
+    else
+    {
+        leftPID = distance;
+        rightPID = distance;
+    }
+
+    for (int i = 10; i > 0; i--)
+    {
+        chassis.drive_set(leftPID * i, rightPID * i);
+    }
+}
+extern ez::PID drivePID;
+void DriveWait();
+void DriveTask();
